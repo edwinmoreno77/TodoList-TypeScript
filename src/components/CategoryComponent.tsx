ad31c8872c67cc7132111302c151b917cc788f22
Edwin Moreno <edwinmoreno77@gmail.com>
2025-01-21 09:07:39 -0300

    CategoryComponent created

---
+++ src/components/CategoryComponent.tsx
@@ -0,0 +1,49 @@
+import { FC } from "react";
+import { Todo } from "../hooks/useTodo";
+
+interface Props {
+  todosToShow: (category: string) => void;
+  completeTodos: Todo[];
+  pendingTodos: Todo[];
+  activeCategory: string;
+  todos: Todo[];
+}
+
+export const CategoryComponent: FC<Props> = ({
+  todosToShow,
+  activeCategory,
+  todos,
+  completeTodos,
+  pendingTodos,
+}) => {
+  return (
+    <section className="flex justify-between px-5 py-2 font-extralight italic text-tiny sm:text-sm w-full bg-slate-900 rounded-b-xl">
+      <span
+        onClick={() => todosToShow("todos")}
+        className={`${
+          activeCategory === "todos" ? "text-lime-500 font-bold" : "text-white"
+        } cursor-pointer hover:brightness-150 hover:scale-110 transition-all ease-in-out duration-200`}
+      >
+        Todas: {todos.length}
+      </span>
+      <span
+        onClick={() => todosToShow("complete")}
+        className={`${
+          activeCategory === "complete"
+            ? "text-lime-500 font-bold"
+            : "text-white"
+        } cursor-pointer hover:brightness-150 hover:scale-110 transition-all ease-in-out duration-200`}
+      >
+        Completadas: {completeTodos.length}
+      </span>
+      <span
+        onClick={() => todosToShow("pending")}
+        className={`${
+          activeCategory === "pending" ? "text-red-500 font-bold" : "text-white"
+        } cursor-pointer hover:brightness-150 hover:scale-110 transition-all ease-in-out duration-200`}
+      >
+        Pendientes: {pendingTodos.length}
+      </span>
+    </section>
+  );
+};
