60735a620b5c889f1342cee44b698bedce83f239
Edwin Moreno <edwinmoreno77@gmail.com>
2025-01-21 13:24:58 -0300

    CAtegoryComponent fixed

--- src/components/CategoryComponent.tsx
+++ src/components/CategoryComponent.tsx
@@ -18,15 +18,17 @@ export const CategoryComponent: FC<Props> = ({
 }) => {
   return (
     <section className="flex justify-between px-5 py-2 font-extralight italic text-tiny sm:text-sm w-full bg-slate-900 rounded-b-xl">
-      <span
+      <button
+        type="button"
         onClick={() => todosToShow("todos")}
         className={`${
           activeCategory === "todos" ? "text-lime-500 font-bold" : "text-white"
         } cursor-pointer hover:brightness-150 hover:scale-110 transition-all ease-in-out duration-200`}
       >
         Todas: {todos.length}
-      </span>
-      <span
+      </button>
+      <button
+        type="button"
         onClick={() => todosToShow("complete")}
         className={`${
           activeCategory === "complete"
@@ -35,15 +37,16 @@ export const CategoryComponent: FC<Props> = ({
         } cursor-pointer hover:brightness-150 hover:scale-110 transition-all ease-in-out duration-200`}
       >
         Completadas: {completeTodos.length}
-      </span>
-      <span
+      </button>
+      <button
+        type="button"
         onClick={() => todosToShow("pending")}
         className={`${
           activeCategory === "pending" ? "text-red-500 font-bold" : "text-white"
         } cursor-pointer hover:brightness-150 hover:scale-110 transition-all ease-in-out duration-200`}
       >
         Pendientes: {pendingTodos.length}
-      </span>
+      </button>
     </section>
   );
 };
